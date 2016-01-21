737fa6664b7532c0a033d57b27bd309c9ccc07c9
RandomVoat <randomvoat@gmail.com>
2016-01-21 00:22:20 -0500

    Adjusted file write, added call to ruby script

--- GetTopPost.py
+++ GetTopPost.py
@@ -1,14 +1,17 @@
 # Returns post content to TopPostOnVoat.py
 
 import time
+import subprocess
 
 # Post/log files
 TOP_POST = [line.rstrip('\n') for line in open('/home/pi/TopPostOnVoat/topPost.txt', 'r')]
 LAST_POST = [line.rstrip('\n') for line in open('/home/pi/TopPostOnVoat/lastPost.txt', 'r+b')]
-#WRITE_LAST = open('/home/pi/TopPostOnVoat/lastPost.txt', 'r+b')
-
+CMD = "ruby /home/pi/TopPostOnVoat/scraper_voat_top.rb"
 
 def main():
+
+    subprocess.call(CMD, shell=True)
+
     # Sets post content
     title = TOP_POST[0]
     href = TOP_POST[1]
@@ -16,7 +19,8 @@ def main():
     
     if href != last:
         # Add current post to duplicate-avoid file
-        LAST_POST.write(href)
+        file = open('/home/pi/TopPostOnVoat/lastPost.txt', 'w')
+        file.write(href)
         # Check char length - twitter max: 140 - shorten title if > 110 char
         if len(title) <= 110:
             status = title + ' - ' +  href
